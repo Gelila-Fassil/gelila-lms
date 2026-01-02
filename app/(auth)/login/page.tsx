9c566e34fa599fed2676323da408c6cfc6284e92
Gelila-Fassil <gelilafassil659@gmail.com>
2026-01-02 16:56:49 +0300

    first commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { GithubIcon } from "lucide-react";
+
+export default function LoginPage(){
+    return(
+        <Card>
+            <CardHeader>
+                 <CardTitle className="text-xl">
+                WELCOME BACK
+            </CardTitle>
+            <CardDescription>
+                Login with your Github or Email account
+            </CardDescription>
+
+            </CardHeader>
+            <CardContent>
+                <Button className="w-full" variant="outline">
+                    <GithubIcon className="size-4"/>
+                    Sign in with Github
+                </Button>
+            </CardContent>
+           
+        </Card>
+    )
+}
